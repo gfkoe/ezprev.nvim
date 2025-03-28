6aa4ae844cf45354d3306c18a285e311185c9ec9
Gabriel Koeb <gabrielkoeb@gmail.com>
2025-03-28 14:19:36 -0400

    got to work, thank you to rmagatti and goto-preview for the inspiration

--- lua/ezprev.lua
+++ lua/ezprev.lua
@@ -4,38 +4,6 @@ M.setup = function()
 	--nothing rn
 end
 
-local process = function(method)
-	if not method then
-		return
-	end
-	local data = method[1] or method
-	local target = nil
-	local pos = {}
-	local uri = data.targetUri or data.uri
-	local range = data.targetRange or data.range
-
-	if uri and range then
-		target = uri
-		pos.start = range.start.line + 1
-		pos["end"] = range.start.character
-	end
-	M.open_window(target, pos)
-end
-
-M.get_handler = function(method)
-	for k, v in pairs(vim.lsp.handlers) do
-		if string.find(k, "textDocument") and type(v) == "function" then
-			return process(method)
-		end
-	end
-end
-
-M.get_definition = function()
-	local params = vim.lsp.util.make_position_params()
-	local method = "textDocument/implementation"
-	pcall(vim.lsp.buf_request, 0, method, params, M.get_handler(method))
-end
-
 M.open_window = function(def, pos)
 	local buf = type(def) == "string" and vim.uri_to_bufnr(def) or def
 
@@ -53,6 +21,51 @@ M.open_window = function(def, pos)
 
 	local window = vim.api.nvim_open_win(buf, true, opts)
 	vim.api.nvim_win_set_cursor(window, pos)
+end
+
+M.get_config = function(data)
+	local uri = data.targetUri or data.uri
+	local range = data.targetRange or data.range
+
+	return uri, { range.start.line + 1, range.start.character }
+end
+
+local process = function(result)
+	if not result then
+		return
+	end
+	local data = result[1] or result
+	local target = nil
+	local pos = {}
+
+	if vim.tbl_isempty(data) then
+		print("No definition found")
+		return
+	end
+
+	target, pos = M.get_config(data)
+
+	M.open_window(target, pos)
+end
+
+local processer = function(method)
+	return function(_, result, _)
+		process(result)
+	end
+end
+
+M.get_handler = function(method)
+	for k, v in pairs(vim.lsp.handlers) do
+		if string.find(k, "textDocument") and type(v) == "function" then
+			return processer(method)
+		end
+	end
+end
+
+M.get_definition = function()
+	local params = vim.lsp.util.make_position_params()
+	local method = "textDocument/implementation"
+	pcall(vim.lsp.buf_request, 0, method, params, M.get_handler(method))
 end
 
 vim.api.nvim_set_keymap(
