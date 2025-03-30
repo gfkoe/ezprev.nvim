404d528ad8c4e9c5e6bb3ad65899331318d4b94d
Gabriel Koeb <gabrielkoeb@gmail.com>
2025-03-30 18:18:20 -0400

    Refactor to make simpler

--- lua/ezprev.lua
+++ lua/ezprev.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.setup = function()
-	--nothing rn
 	M.use_keymaps()
 end
 
@@ -9,7 +8,7 @@ M.open_window = function(def, pos)
 	local buf = type(def) == "string" and vim.uri_to_bufnr(def) or def
 
 	local width = math.floor(vim.o.columns * 0.4)
-	local height = math.floor(vim.o.lines * 0.4)
+	local height = math.floor(vim.o.lines * 0.6)
 	local opts = {
 		relative = "cursor",
 		width = width,
@@ -31,34 +30,30 @@ M.get_config = function(data)
 	return uri, { range.start.line + 1, range.start.character }
 end
 
-local process = function(result)
-	if not result then
-		return
-	end
-	local data = result[1] or result
-	local target = nil
-	local pos = {}
-
-	if vim.tbl_isempty(data) then
-		print("No definition found")
-		return
-	end
-
-	target, pos = M.get_config(data)
-
-	M.open_window(target, pos)
-end
-
-local processer = function()
+local process = function()
 	return function(_, result, _)
-		process(result)
+		if not result then
+			return
+		end
+		local data = result[1] or result
+		local target = nil
+		local pos = {}
+
+		if vim.tbl_isempty(data) then
+			print("No definition found")
+			return
+		end
+
+		target, pos = M.get_config(data)
+
+		M.open_window(target, pos)
 	end
 end
 
 M.get_handler = function()
 	for k, v in pairs(vim.lsp.handlers) do
 		if string.find(k, "textDocument") and type(v) == "function" then
-			return processer()
+			return process()
 		end
 	end
 end
